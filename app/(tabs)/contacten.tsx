3918b881641bcf5d04ae45ebab8a2dac886472ec
LotDW5 <lottedewulf@gmail.com>
2026-08-07 12:08:38 +0200

    chore: push local UI header adjustments

--- app/(tabs)/contacten.tsx
+++ app/(tabs)/contacten.tsx
@@ -268,7 +268,7 @@ const styles = StyleSheet.create<any>({
     overflow: 'hidden',
   },
   pageHeader: {
-    marginTop: 144,
+    marginTop: 86,
     marginBottom: 24,
     paddingHorizontal: 24,
     zIndex: 20,
